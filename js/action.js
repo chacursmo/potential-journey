3eab992742d83f6a9971a5ee02f1f7568511df15
Sam Crouch <chacursmo@gmail.com>
2017-02-22 13:34:54 -0600

    FEATURE: Is workable, to be refactored...

--- js/action.js
+++ js/action.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
 	$("#texty").keypress(function(event){
 	    var keycode = (event.keycode ? event.keyCode : event.which);
 	    if (keycode == '13'){
-		var name = "https://en.wikipedia.org/w/api.php?action=query&titles=";
-		var rest = "&prop=revisions&rvprop=content&format=json"
+		var name = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=";
+		var rest = "&format=json&formatversion=2&callback=?"
 		var middle = $("#texty").val();
 		var puttogether = name + middle + rest;
 		puttogether = puttogether.replace(/(\r\n|\n|\r)/gm,"");
@@ -14,10 +14,16 @@ $(document).ready(function(){
 		    url :puttogether,
 		    dataType: "jsonp",
 		    success : function(data){
-			var newhtml = "<div>"+data.query+"</div>";
-			$(".theAction").append(newhtml);
-			console.log(data.query);
-			console.log(puttogether);
+			var poo = data.query.search;
+			for (var ii = 0; ii < poo.length;ii++){ 
+			    var toadd = poo[ii].snippet;
+			    var morbo = poo[ii].title.replace(/ /g, "_");
+			    toadd = "<a href=\"https://en.wikipedia.org/wiki/"+morbo+"\">"+toadd+"</a>";
+			    $(".mongogg").append(toadd);
+			    $(".mongogg").append("<br><br>");
+			console.log(poo[ii]);
+
+			}
 
 		    },
 		    error : function (e){
@@ -30,3 +36,5 @@ $(document).ready(function(){
 	});
     });
 });
+
+
